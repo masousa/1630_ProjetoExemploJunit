8393a1197b626051cb3d793fc7af46d3b360eb35
Matheus Sousa <alvessousam6@gmail.com>
2026-04-14 21:59:09 -0300

    executando testes unitários em conjunto

--- src/test/java/br/personalsamples/temp/CalculadoraTest.java
+++ src/test/java/br/personalsamples/temp/CalculadoraTest.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -43,4 +48,22 @@ class CalculadoraTest {
 	void shouldThrownAnErrorWhenSumWithTheMaxIntegerValue(){
 		assertFalse(calculadora.soma(50, Integer.MAX_VALUE) > 0);
 	}
+
+	@Test
+	void shouldMultiplyWithNoErrors(){
+		assertEquals(40, calculadora.multiplicacao(5,8));
+		assertEquals(-24, calculadora.multiplicacao(-3, 8));
+	}
+	@ParameterizedTest
+	@MethodSource(value="streamNumbers")
+	void shouldMultiplyBunchOfNumbersWithNoErrors(int expected, int number1, int number2){
+		assertEquals(expected, calculadora.multiplicacao(number1, number2));
+	}
+	private static Stream<Arguments> streamNumbers(){
+		return Stream.of(
+				Arguments.of(40,5,8),
+				Arguments.of(-24,-3,8)
+		);
+	}
+
 }
